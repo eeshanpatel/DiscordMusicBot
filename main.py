2211dba33ec108b33daf80e7ed77bc452b752353
eeshanpatel <88124013+eeshanpatel@users.noreply.github.com>
2022-08-17 00:19:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ client = commands.Bot(command_prefix='?', intents = discord.Intents.all())
 for i in range(len(cogs)):
 	cogs[i].setup(client)
 
-client.run("MTAwOTI0ODM3NTMyMjk3NjI1Ng.G108Id.yW5_dd82LgGNH-p24LjeYxd-u0XSJaiEUdcnss")
+client.run("")
